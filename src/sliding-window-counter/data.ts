9351f473ab184c14f28877ed9aca84669ead9bec
pqhung3007 <hungpham307@gmail.com>
2025-03-12 20:13:28 +0700

    add sliding window counter

--- src/sliding-window-counter/data.ts
+++ src/sliding-window-counter/data.ts
@@ -4,5 +4,5 @@ type RequestLog = {
 }
 
 export const requestThreshold = 10
-export const slidingWindowInMs = 60 * 1000 // 60 seconds
+export const slidingWindowInMs = 60 * 1000
 export const requestLogs = new Map<string, RequestLog>()
